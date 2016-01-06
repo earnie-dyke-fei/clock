fd4803e2417f27d36ff3fd78e969c32cca23f99c
Earnest Dyke <earnest.dyke@ferguson.com>
2016-01-06 13:34:59 -0500

    Initial commit

---
+++ src/main/java/com/ferguson/clock/Clock.java
@@ -0,0 +1,18 @@
+package com.ferguson.clock;
+
+/**
+ * Interface for System Clock
+ * 
+ * @author E Dyke (AAI6838)
+ *
+ */
+public interface Clock {
+
+	/**
+	 * Get current system time in millis
+	 * 
+	 * @return current system time in millis
+	 */
+	public long getTime();
+
+}
